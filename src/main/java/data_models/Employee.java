3ce4adbb7c180a343efc1691dc2e18d78daec2ee
ali <ali.kaya94@web.de>
2018-04-17 17:08:19 +0200

    adjust types in classes
    types werent same in db, so changed them for being able to make sql
    statements
    
    [3665]

--- src/main/java/data_models/Employee.java
+++ src/main/java/data_models/Employee.java
@@ -4,13 +4,41 @@ import java.util.UUID;
 
 public class Employee extends Person {
 
-	private UUID employeeId;
-	// private PermisionEnum permission;		// has to be generated
-	public Position m_Position;
-	public LoginCredentials m_LoginCredentials;
+	private int employeeId;
+	private char isDeleted;
+	private int positionId;
 
 	public Employee(){
 
 	}
 
+	public Employee(int employeeId, char isDeleted, int positionId) {
+		this.employeeId = employeeId;
+		this.isDeleted = isDeleted;
+		this.positionId = positionId;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public char getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(char isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public int getPositionId() {
+		return positionId;
+	}
+
+	public void setPositionId(int positionId) {
+		this.positionId = positionId;
+	}
 }
