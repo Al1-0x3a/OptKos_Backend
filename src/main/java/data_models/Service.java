249a11291cdb4ecede6741c24df2e2bf060d2959
ali <ali.kaya94@web.de>
2018-04-25 15:57:26 +0200

    Add Service Dao
    [No Reference]
    
    Signed-off-by: ali <ali.kaya94@web.de>

--- src/main/java/data_models/Service.java
+++ src/main/java/data_models/Service.java
@@ -1,21 +1,79 @@
 package data_models;
 
 import java.math.BigDecimal;
+import java.sql.Time;
 import java.time.Duration;
 import java.util.UUID;
 
 public class Service {
 
-	private String description;
-	private Duration durationCalculated;
-	private Duration durationStatic;
-	private String name;
-	private BigDecimal price;
 	private UUID serviceId;
-	// public ServiceList m_ServiceList;				// has to be generated
+	private String name;
+	private String description;
+	private BigDecimal price;
+	private Time durationPlanned;
+	private Time durationAverage;
 
 	public Service(){
-
+this.serviceId = UUID.randomUUID();
 	}
 
+	public Service(UUID serviceId, String name, String description, BigDecimal price,
+	Time durationPlanned, Time durationAverage) {
+		this.serviceId = serviceId;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.durationPlanned = durationPlanned;
+		this.durationAverage = durationAverage;
+	}
+
+
+	public UUID getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(UUID serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Time getDurationPlanned() {
+		return durationPlanned;
+	}
+
+	public void setDurationPlanned(Time durationPlanned) {
+		this.durationPlanned = durationPlanned;
+	}
+
+	public Time getDurationAverage() {
+		return durationAverage;
+	}
+
+	public void setDurationAverage(Time durationAverage) {
+		this.durationAverage = durationAverage;
+	}
 }
