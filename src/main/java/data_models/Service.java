0107ab1ac087aecac8ae32d33efdf1fa337dfdd1
AndreRehle <38333987+AndreRehle@users.noreply.github.com>
2018-05-04 09:44:25 +0200

    Add service dao (#13)
    * Add Service Dao
    
    [No Reference]
    
    Signed-off-by: ali <ali.kaya94@web.de>
    
    * Include Delete option in ServiceDao.

--- src/main/java/data_models/Service.java
+++ src/main/java/data_models/Service.java
@@ -1,6 +1,7 @@
 package data_models;
 
 import java.math.BigDecimal;
+import java.sql.Time;
 import java.time.Duration;
 import java.util.UUID;
 
@@ -9,11 +10,75 @@ public class Service {
 	private String name;
 	private String description;
 	private BigDecimal price;
-	private Duration durationPlanned;
-	private Duration durationAverage;
+	private Time durationPlanned;
+	private Time durationAverage;
+	private String isDeleted;
 
 	public Service(){
-
+this.serviceId = UUID.randomUUID();
 	}
 
+	public Service(UUID serviceId, String name, String description, BigDecimal price,
+	Time durationPlanned, Time durationAverage, String isDeleted) {
+		this.serviceId = serviceId;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.durationPlanned = durationPlanned;
+		this.durationAverage = durationAverage;
+		this.isDeleted = isDeleted;
+	}
+
+
+	public UUID getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(UUID serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Time getDurationPlanned() {
+		return durationPlanned;
+	}
+
+	public void setDurationPlanned(Time durationPlanned) {
+		this.durationPlanned = durationPlanned;
+	}
+
+	public Time getDurationAverage() {
+		return durationAverage;
+	}
+
+	public void setDurationAverage(Time durationAverage) {
+		this.durationAverage = durationAverage;
+	}
+
+	public String getIsDeleted(){ return isDeleted; }
+
+	public void setIsDeleted(String isDeleted) { this.isDeleted = isDeleted; }
 }
