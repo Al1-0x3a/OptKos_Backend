5ff4a520832f2012f8b10bd6bc6a5c851a0e316e
MiSzostak <szostak.michael@gmail.com>
2018-06-02 15:54:28 +0200

    publish statistics endpoint

--- src/main/java/client_api/StatisticApi.java
+++ src/main/java/client_api/StatisticApi.java
@@ -1,26 +1,23 @@
 package client_api;
 
-import data_loader.data_access_object.*;
 import data_models.*;
 import manager.StatisticManager;
 
 import javax.jws.WebService;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 @SuppressWarnings("ValidExternallyBoundObject")
-@WebService(endpointInterface = "client_api.IAdministrativeApi")
+@WebService(endpointInterface = "client_api.IStatisticApi")
 public class StatisticApi implements IStatisticApi {
     private final StatisticManager statisticManager;
 
     public StatisticApi() {
         statisticManager = new StatisticManager();
     }
-
-
+    
     @Override
-    public List<ServiceCounter> getAllServiceCounter(LocalDateTime starttime, LocalDateTime endtime) {
-        return statisticManager.getAllServiceCounter(starttime, endtime);
+    public List<ServiceCounter> getAllServiceCounter(LocalDateTime startTime, LocalDateTime endTime) {
+        return statisticManager.getAllServiceCounter(startTime, endTime);
     }
 }
