b64d23df3e2910c83aa12cf07b77cf004db32ab8
ali <ali.kaya94@web.de>
2018-06-12 14:29:16 +0200

    Add initial method for calculation of worktime statistic
    Signed-off-by: ali <ali.kaya94@web.de>

--- src/main/java/manager/StatisticManager.java
+++ src/main/java/manager/StatisticManager.java
@@ -3,6 +3,7 @@ package manager;
 import data_loader.data_access_object.ServiceCounterDao;
 import data_models.ServiceCounter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class StatisticManager {
@@ -10,5 +11,10 @@ public class StatisticManager {
     public ServiceCounter getAllServiceCounter(String serviceId, String startTime, String endTime) {
         return ServiceCounterDao.getAllServiceCounter(serviceId, LocalDateTime.parse(startTime),
                 LocalDateTime.parse(endTime));
+    }
+
+    public void getWorktimeStatistics(String start, String end){
+        LocalDate startDate = LocalDate.parse(start);
+        LocalDate endDate = LocalDate.parse(end);
     }
 }
